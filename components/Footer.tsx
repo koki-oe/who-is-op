28730d9692581740872ce59b00d27f92b35ce9f6
koki-oe <program.koki@gmail.com>
2022-01-30 07:46:17 +0900

    fix: Fix the copyright

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,7 +2,7 @@ const Footer = () => {
   return (
     <footer className="relative pt-36 w-full">
       <div className="relative mx-auto" style={{width: "fit-content"}}>
-        <span>&copy; 2021 - 2022 Ponkotate</span>
+        <span>&copy; 2021 - 2022 Koki.O</span>
       </div>
     </footer>
   )
